522e3d362d86d40849eae2336de303cc19eed7b7
Steve <steve.newstead@gmail.com>
2023-12-02 15:27:31 +0000

    Finished day 1, hacky but such is life

--- day-1/Program.cs
+++ day-1/Program.cs
@@ -14,7 +14,7 @@ class Program
         string fileName = Console.ReadLine() ?? "";
 
         if (fileName == string.Empty) {
-            fileName = "test-input.txt";
+            fileName = "full-input.txt";
         }
 
         using (StreamReader reader = new StreamReader($"input-files/{fileName}"))
@@ -22,39 +22,43 @@ class Program
             string? line = "";
             while ((line = reader.ReadLine()) != null) {
                 count++;
-                var firstAndLastNumberCombined = GetTheFirstAndLastNumber(line);
-                Console.WriteLine($"Line number: {count} Line output: {firstAndLastNumberCombined}");
-                sum += firstAndLastNumberCombined;
+                MatchCollection numbers = GetMatches(line);
+                if (numbers.Count > 0) {
+                    var firstAndLastNumberCombined = GetTheFirstAndLastNumber(line, numbers);
+                    //Console.WriteLine($"Line number: {count} Line output: {firstAndLastNumberCombined}");
+                    sum += firstAndLastNumberCombined;
+                }
             }
         }
 
-        Console.WriteLine(sum);
+        Console.WriteLine("Total :" + sum);
     }
 
-    private static int GetTheFirstAndLastNumber(string line) {
-       // var pattern = @"\d+";
-        var pattern =  @"(?:one|two|three|four|five|six|seven|eight|nine|\d+)";
-        //var patternFromGPT = @"\b(?:one|two|three|four|five|six|seven|eight|nine|\d+)\b";
-        var numbers = Regex.Matches(line, pattern);
-        int firstDigit = 0;
-        int lastDigit = 0;
+    public static int GetTheFirstAndLastNumber(string line, MatchCollection numbers)
+    {
+        int firstDigit;
+        int lastDigit;
 
-        
-        var fullFirstNumber = GetNumberFromText(numbers[0].Value, true);
-        var fullLastNumber = GetNumberFromText(numbers[numbers.Count -1].Value, false);
+        var fullFirstNumber = GetNumberFromText(numbers[0].Value, line, true);
+        var fullLastNumber = GetNumberFromText(numbers[numbers.Count - 1].Value, line, false);
         if (fullFirstNumber == null || fullLastNumber == null)
             throw new IndexOutOfRangeException();
 
-        firstDigit = int.Parse(fullFirstNumber.Substring(0,1));
-    
+        firstDigit = int.Parse(fullFirstNumber.Substring(0, 1));
+
         if (numbers.Count == 1)
         {
-            if (fullFirstNumber.Length > 1) {
+            if (fullFirstNumber.Length > 1)
+            {
                 lastDigit = int.Parse(fullFirstNumber.Substring(fullFirstNumber.Length - 1));
-            } else {
+            }
+            else
+            {
                 lastDigit = firstDigit;
             }
-        } else {
+        }
+        else
+        {
             lastDigit = int.Parse(fullLastNumber.Substring(fullLastNumber.Length - 1));
         }
 
@@ -62,7 +66,9 @@ class Program
 
     }
 
-    private static string? GetNumberFromText(string number, bool isFirstValue) {
+   
+
+    private static string? GetNumberFromText(string number, string inputLine, bool isFirstValue) {
         
         int a;
 
@@ -70,9 +76,13 @@ class Program
         {
             return number;
         }
-        string lowerCaseNumber = number.ToLower();
 
-        switch(lowerCaseNumber) {
+        if (!isFirstValue)
+        {
+            number = EdgeCaseForMissingLastNumber(number, inputLine);
+        }
+
+        switch (number.ToLower()) {
             case "one":
                 return "1";
             case "two":
@@ -96,5 +106,38 @@ class Program
         return null;
     }
 
+    /// <summary>
+    /// hack to work around edge case
+    /// in an example where the sequene is '126dzbvg6two4oneightntd' the regex will find 'one'
+    /// but not 'eight', as 'oneight' shares the same last letter
+    /// so here we just check if we really have found the last number.
+    /// yeah, I should have written some tests, it would have said me a lot of hassle.
+    /// </summary>
+    /// <param name="number"></param>
+    /// <param name="inputLine"></param>
+    /// <returns></returns>
+    private static string EdgeCaseForMissingLastNumber(string number, string inputLine)
+    {
+        var positionOfEndOfThelastNumberFoundAsTextExlcludingLastLetter = inputLine.LastIndexOf(number) + number.Length - 1;
+        var remainingInputFile = inputLine.Substring(positionOfEndOfThelastNumberFoundAsTextExlcludingLastLetter, inputLine.Length - positionOfEndOfThelastNumberFoundAsTextExlcludingLastLetter);
+        //still might be wrong if there are few in the string, but lets try this
+        var lastNumberInInputFile = GetMatches(remainingInputFile);
+
+        if (lastNumberInInputFile.Count > 0  && lastNumberInInputFile[0].Value != number)
+        {
+            number = lastNumberInInputFile[0].Value;
+            //Console.WriteLine("Here is a problem line " + inputLine);
+
+        }
+        return number;
+    }
+
+    private static MatchCollection GetMatches(string line)
+    {
+        var pattern = @"(?:one|two|three|four|five|six|seven|eight|nine|\d+)";
+        //var patternFromGPT = @"\b(?:one|two|three|four|five|six|seven|eight|nine|\d+)\b";
+        var numbers = Regex.Matches(line, pattern);
+        return numbers;
+    }
     
 }
