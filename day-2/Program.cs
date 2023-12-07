9546ec02ca4b172745ef20f6fd8e5319a09e8aab
Steve Newstead <snewstead@macmillan.org.uk>
2023-12-07 12:06:11 +0000

    day 2 complete

--- day-2/Program.cs
+++ day-2/Program.cs
@@ -1,77 +1,77 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Linq.Expressions;
 using System.Text.RegularExpressions;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace Day2;
 
 public class Program
 {
+
+    
+
     static void Main(string[] args)
     {
-        
-
         var sum = 0;
         var count = 0;
+        
         Console.WriteLine("Input please: ");
         string fileName = Console.ReadLine() ?? "";
 
         if (fileName == string.Empty)
         {
-            fileName = "full-input.txt";
+            fileName = "full";
         }
 
-
-        using (StreamReader reader = new StreamReader($"input-files/{fileName}"))
+        using (StreamReader reader = new StreamReader($"input-files/{fileName}-input.txt"))
         {
-            string? line = "";
-            while ((line = reader.ReadLine()) != null)
+            string? game = "";
+            while ((game = reader.ReadLine()) != null)
             {
                 count++;
-                if (IsValidGame(line))
-                {
-                    sum += count;
-                }
+                sum += GetCubeGames(game);
             }
         }
-        Console.WriteLine(sum.ToString());
+
+        Console.WriteLine("The total is " + sum);
+        
     }
 
-    public static bool IsValidGame(string game)
+    public static int GetCubeGames(string game)
     {
+        int redCubes = 0;
+        int greenCubes = 0;
+        int blueCubes = 0;
 
-        int maxRedCubes = 12;
-        int maxGreenCubes = 13;
-        int maxBlueCubes = 14;
-
-        var lines = game.Split(';');
         string pattern = @"(\d+)\s*([a-zA-Z]+)";
+              
+        MatchCollection matches = Regex.Matches(game, pattern);
 
-        foreach (string line in lines)
+        foreach (Match match in matches)
         {
-            List<string> cubes = new List<string>();
-            MatchCollection matches = Regex.Matches(line, pattern);
+            int number = Int32.Parse(match.Groups[1].Value);
+            string colour = match.Groups[2].Value;
 
-            foreach (Match match in matches)
+            switch (colour.ToLower())
             {
-                int number = Int32.Parse(match.Groups[1].Value);
-                string colour = match.Groups[2].Value;
-
-                for (int i = 0; i < number; i++)
-                {
-                    cubes.Add(colour);
-                }
+                case "blue":
+                    if (number > blueCubes)
+                        blueCubes = number;
+                    break;
+                case "green":
+                    if (number > greenCubes)
+                        greenCubes = number;
+                    break;
+                default:
+                    if (number > redCubes)
+                        redCubes = number;
+                    break;
             }
-            var tooManyRedCubes = cubes.Count(x => x == "red") > maxRedCubes;
-            var tooManyGreenCubes = cubes.Count(x => x == "green") > maxGreenCubes;
-            var tooManyBlueCubes = cubes.Count(x => x == "blue") > maxBlueCubes;
 
-            if (tooManyRedCubes || tooManyGreenCubes || tooManyBlueCubes)
-            {
-                return false;
-            }
         }
 
-        return true;
-    }
-    
+        return redCubes * blueCubes * greenCubes;
+
+    }    
 }
